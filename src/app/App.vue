6ddfde21123a10f207069322c85e07c9b337217b
Ivan <ivanitit3535@gmail.com>
2026-03-23 12:23:56 +0200

    feat: sortBy

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,26 +1,44 @@
 <script setup>
-import { onMounted, provide, ref } from 'vue';
-import { fetchProducts } from '@/entities/product/api';
+import { onMounted, provide, ref, watch } from 'vue';
+import axios from 'axios';
+import { BASE_URL, PRODUCTS_ENDPOINT } from '@/shared/api/config';
 import { usePageNavigation } from '@/shared/hooks/usePageNavigation';
 import Home from '@/pages/home-page';
 import './styles/main.scss';
 // перенести сюда импорт router
 
 const items = ref([]);
+const sortBy = ref('');
+// const searchQuery = ref('');
 
-const loadData = async () => {
+const onChangeSelect = (event) => {
+  console.log(event.target.value);
+  sortBy.value = event.target.value;
+};
+
+onMounted(async () => {
   try {
-    items.value = await fetchProducts();
+    const { data } = await axios.get(`${BASE_URL}${PRODUCTS_ENDPOINT}`);
+    items.value = data;
     console.log('Data loaded successfully:', items.value);
   } catch (err) {
     console.error('Error loading data:', err.message);
   }
-};
+});
 
-onMounted(loadData);
+watch(sortBy, async () => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}${PRODUCTS_ENDPOINT}?sortBy=${sortBy.value}`);
+    items.value = data;
+    console.log('Data sort loaded successfully:', items.value);
+  } catch (err) {
+    console.error('Error loading data sort:', err.message);
+  }
+});
 
 usePageNavigation();
 provide('items', items);
+provide('onChangeSelect', onChangeSelect);
 </script>
 
 <template>
